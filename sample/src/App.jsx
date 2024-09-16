b80cb51dc586b4184ef13e0dc9b62a720d669432
Sudharsan V <176049812+Sudharsanv151@users.noreply.github.com>
2024-09-16 15:20:19 +0530

    Login page using React.js and Tailwind css

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+     <div className="h-screen w-screen flex flex-col justify-center items-center bg-gray-200 ">
+      <div className="h-3/6 w-[30%]  flex flex-col justify-center items-center bg-gradient-to-tr-100 rounded-xl shadow-xl">
+      <h1 className="p-1 h-2/2 w-2/1 text-xl flex justify-center items-center w-full font-bold size-2"> LOGIN </h1>
+      <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
+        <input type="text" name="" id="" placeholder="Name"  className="p-3 bg-[#f8f8f8] w-full font-bold outline-green-200 active:outline-green-500 focus:border-b-2 "/>
+        <input type="number" name="" id="" placeholder="Mobile"className="p-3 bg-[#f8f8f8] w-full font-bold outline-green-200 active:outline-green-500 focus:border-b-2 " />
+        <input type="password" name="" id="" placeholder="Password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-green-200 active:outline-green-300 focus:border-b-2"/>
+        <button type="submit" className="w-full bg-green-600 text-white p-3 outline-black">Register</button>
+      </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
 
-export default App
+export default App;
